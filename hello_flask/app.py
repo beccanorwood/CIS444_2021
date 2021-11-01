b9a0243343c10b036b5ac0b895f66ba72561a58f
Ubuntu <ubuntu@ip-172-31-30-224.ec2.internal>
2021-11-01 05:46:41 +0000

    Purchased Books table updated with date/time of purchase

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -42,8 +42,8 @@ def index():
 @app.route('/signup', methods=['POST'])
 def signup():
     
-    username = request.form['username']
-    password = request.form['password']
+    name = request.form['username']
+    pwd = request.form['password']
 
     cursor = global_db_con.cursor()
 
@@ -51,18 +51,18 @@ def signup():
     print(password)
 
 
-    cursor.execute("SELECT * FROM users WHERE username = %s", (username,)) #to check if username is available 
-    salted_password = bcrypt.hashpw( bytes(password, 'utf-8'), bcrypt.gensalt(10)) #encrypt password 
+    cursor.execute("SELECT * FROM users WHERE username = %s", (name,)) #to check if username is available 
+    salted_password = bcrypt.hashpw( bytes(pwd, 'utf-8'), bcrypt.gensalt(10)) #encrypt password 
     newuser = cursor.fetchone()
 
     if newuser is None:
-        cursor.execute("INSERT into users (username, password) VALUES (%s, %s);", (username, salted_password.decode('utf-8'),)) #insert new user with encrypted password to db
+        cursor.execute("INSERT into users (username, password) VALUES (%s, %s);", (name, salted_password.decode('utf-8'),)) #insert new user with encrypted password to db
         global_db_con.commit() #update db
         cursor.close() 
+
         #jwt with an expiration time of 30 minutes 
         #user_jwt = jwt.encode({"user": username, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, JWT_SECRET, algorithm="HS256")
-        #user_jwt = jwt.encode({'user': username}, JWT_SECRET, algorithm="HS256")
-        user_jwt = createToken(username, password)
+        user_jwt = JWT(name, pwd)
         return jsonify({'validJWT': True, 'message': user_jwt})
     else:
         cursor.close()
@@ -74,13 +74,13 @@ def signup():
 def login():
     #only validate JWT for user when they login since they will have valid one when they sign in?
     #save username & password from frontend and user those to retrieve token
-    username = request.form['username']
-    password = request.form['password']
+    name = request.form['username']
+    pwd = request.form['password']
 
 
     #decode jwt and compare to username and password in db
     cursor = global_db_con.cursor()
-    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+    cursor.execute("SELECT * FROM users WHERE username = %s", (name,))
     record = cursor.fetchone()
 
     if record is None:
@@ -92,18 +92,17 @@ def login():
         print("Salted Password: " + salted_password)
         cursor.close()
         #compare password from form with decoded password
-        if (bcrypt.checkpw( bytes(password, 'utf-8'), salted_password.encode() )): 
-            #user_jwt = jwt.encode({'user': username}, JWT_SECRET, algorithm="HS256")
-            user_jwt = createToken(username, password)
+        if (bcrypt.checkpw( bytes(pwd, 'utf-8'), salted_password.encode() )): 
+            user_jwt = JWT(name, pwd)
             return jsonify({'validCreds': True, 'validToken': True, 'message': user_jwt})
         else:
             return jsonify({'validCreds': False, 'validToken': False, 'message': 'Incorrect password. Please try again'})
 
 
 @app.route('/createToken', methods=['GET'])
-def createToken(username, password):
+def JWT(username, password):
     
-    return jwt.encode({'user': username}, JWT_SECRET, algorithm="HS256")
+    return jwt.encode({'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, JWT_SECRET, algorithm="HS256")
     
 
 
@@ -113,16 +112,11 @@ def createToken(username, password):
 def validToken():
 
     token = request.args.get('jwt')
-    #json_data = request.json
-    #token = json_data['jwt']
-    
-    print(token)
 
     if not jwt.decode(token, JWT_SECRET, algorithms=["HS256"]): #user has valid token
         return jsonify({'validToken': False})
     else:
         return jsonify({'validToken': True})
-
 
 
 @app.route('/populateBookList', methods=['GET'])
@@ -139,13 +133,14 @@ def populateBookList():
 
     columns = cursor.description #returns list of tuples where [0] for each is the column header
     result = [{columns[index][0]:column for index, column in enumerate(value)} for value in record]
+    cursor.close()
 
     print(result)
 
     return jsonify({'Books': result})
 
 
-#@app.route('/getBooks', methods=['GET'])
+@app.route('/getBooks', methods=['GET'])
 def getBooks():
     #method that requires valid jwt
     #compare decoded jwt with decoded password matching
@@ -153,22 +148,33 @@ def getBooks():
     #compare decoded with user's name
     #then compare user's password and if it's the same as the decoded password using bcrypt 
 
+    #receive username & pasword
+    #get token from token endpoint
+
+    token = request.args.get('jwt')
     cursor = global_db_con.cursor()
 
-    return json_response(result = jwt.decode(user_jwt, JWT_SECRET, algorithms=['HS256']))
+    isTokenValid = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+    username = isTokenValid['user']
+    print("username decoded" + username)
 
-     #check database to confirm username from database matches password and decoded token matches username
-    cursor.execute("SELECT * FROM users WHERE username = %", (username,))
-    record = cursor.fetchone()
-        
-    username_db = record[0]
 
-    if (username_db == token_username): #check for valid token
-        return ({'message': "You may view the bookstore"})
+    if (isTokenValid):
+        #query username and password in database and select book to purchase 
+        cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        userInfo = cursor.fetchone()
+
+        print(userInfo)
+
+        userid = userInfo[0]
+        print(userid)
+
+        cursor.execute("INSERT into purchased_books (user_id, book_id, purchase_time) VALUES (%s, %s, %s);", (userid, username, datetime.datetime.now(),))
+        global_db_con.commit()
+        cursor.close()
+        return jsonify({'Book_Purchased': True})
     else:
-        return ({'message': "Invalid JWT"});
-
-    
+        return jsonify({'Book_Purchased': False})
 
 
 app.run(host='0.0.0.0', port=80)
