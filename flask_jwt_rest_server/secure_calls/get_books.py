6d10d9ace1da92ade43e3d7a84a1b2f80c15bf85
beccanorwood <rebeccarosenorwood@gmail.com>
2021-11-19 16:47:07 -0800

    Book List retrieving books from db properly

--- flask_jwt_rest_server/secure_calls/get_books.py
+++ flask_jwt_rest_server/secure_calls/get_books.py
@@ -1,11 +1,21 @@
 from flask import request, g
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from tools.token_tools import create_token
+from psycopg2 import sql
 
 from tools.logging import logger
 
 def handle_request():
     logger.debug("Get Books Handle Request")
 
-    return json_response( token = create_token(  g.jwt_data ) , books = {})
+    cursor = g.db.cursor()
+    cursor.execute("SELECT * FROM books")
+    record = cursor.fetchall()
+
+    columns  = cursor.description
+    booklist = [{columns[index][0]:column for index, column in enumerate(value)} for value in record]
+    cursor.close()
+
+
+    return json_response( token = create_token(  g.jwt_data ) , book = booklist)
 
